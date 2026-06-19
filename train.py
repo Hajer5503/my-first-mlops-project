bf0dd07c904c3d7d9bd54f6cdb589458f4b10c20
Hajer5503 <Hajer.Abdelkefi@esprit.tn>
2026-06-19 18:23:19 +0100

    fixing mlflow changes

--- train.py
+++ train.py
@@ -32,18 +32,6 @@ def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE
     )
-<<<<<<< Updated upstream
-    model = RandomForestClassifier(n_estimators=100, random_state=RANDOM_STATE)
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-    acc = accuracy_score(y_test, predictions)
-    logging.info(f"Accuracy: {acc:.2f}")
-
-    os.makedirs("results", exist_ok=True)
-    with open("results/metrics.json", "w") as f:
-        json.dump({"accuracy": round(float(acc), 2)}, f)
-    logging.info("Metrics saved to results/metrics.json")
-=======
 
     with mlflow.start_run():
         model = RandomForestClassifier(
@@ -64,7 +52,6 @@ def train_model(X, y):
         with open("results/metrics.json", "w") as f:
             json.dump({"accuracy": round(float(acc), 2)}, f)
         logging.info("Metrics saved to results/metrics.json")
->>>>>>> Stashed changes
 
     return model
 
