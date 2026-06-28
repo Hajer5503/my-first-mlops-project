39c31327bb9f9f0cf3410f34b64865ab20aad623
Hajer5503 <Hajer.Abdelkefi@esprit.tn>
2026-06-28 12:25:06 +0100

    format code with flake8

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import logging
 import os
 
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
